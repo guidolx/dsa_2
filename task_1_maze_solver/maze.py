0bd369ec7eb0d9dcf1b6b2feda5ab9b02339d9ab
Guido Amabili <guido.amabili@gmail.com>
2024-11-07 11:09:37 +0100

    Modified the print and added more functions

--- task_1_maze_solver/maze.py
+++ task_1_maze_solver/maze.py
@@ -2,8 +2,8 @@ import os
 import random
 
 
-BLOCK = 0
-FREE = 1
+BLOCKED = 0
+EXPLORABLE = 1
 VISITED = 5
 STEP = 2
 
@@ -50,8 +50,10 @@ maze = [
 ] 
 
 
-def available_directions(row, col):
+def available_directions(row, col,maze):
     directions = []
+    # i is the index included in the tuple 
+    # to match the character in the direction string.
     for i in range(4):
         # Find the next row based on the current row (row)
         # and the dr[] array
@@ -62,7 +64,7 @@ def available_directions(row, col):
 
         # Check if the next cell is valid or not
         if is_valid(next_row, next_col, n, maze):
-            directions.append((next_row,next_col))
+            directions.append((next_row,next_col,i))
     return directions
 
 
@@ -71,9 +73,9 @@ def available_directions(row, col):
 # is inside the maze
 # and unblocked (has value 1)
 def is_valid(row, col, n, maze):
-    return 0 <= row < n and 0 <= col < n and maze[row][col] == FREE
+    return 0 <= row < n and 0 <= col < n and maze[row][col] == EXPLORABLE
 
-# Function to get all valid paths
+# Deterministic function to get all valid paths if any
 def find_path(row, col, maze, dest_row,dest_col, solution, current_path):
     # If we reach the destination cell of the matrix, add
     # the current path to solution and return
@@ -81,7 +83,7 @@ def find_path(row, col, maze, dest_row,dest_col, solution, current_path):
         solution.append(current_path)
         return
     # Mark the current cell as blocked
-    maze[row][col] = BLOCK
+    maze[row][col] = BLOCKED
 
     for i in range(4):
         # Find the next row based on the current row (row)
@@ -94,21 +96,22 @@ def find_path(row, col, maze, dest_row,dest_col, solution, current_path):
         # Check if the next cell is valid or not
         if is_valid(next_row, next_col, n, maze):
             current_path += direction[i]
+            visited.append((next_row, next_col))
             # Recursively call the find_path function for
             # the next cell
-            discard = find_path(next_row, next_col, maze, dest_row, dest_col, solution, current_path)
+            find_path(next_row, next_col, maze, dest_row, dest_col, solution, current_path)
             # Remove the last direction when backtracking
             current_path = current_path[:-1]
-            # keep the cell in the visited list
-            if discard == False:
-                visited.append((next_row, next_col))
+            
             
 
     # Mark the current cell as unblocked
-    maze[row][col] = FREE
+    maze[row][col] = EXPLORABLE
 
-# Function to get all valid paths with random moves
-def find_path_lv(row, col, maze, dest_row,dest_col, solution, current_path, count):
+# Las Vegas based function to explore 
+# the maze based on random choice 
+# regarding the direction to take.
+def find_path_lv(row, col, maze, dest_row,dest_col, solution):
     
     max = 400     
     stack = []
@@ -116,13 +119,16 @@ def find_path_lv(row, col, maze, dest_row,dest_col, solution, current_path, coun
     stack.append((row,col))
     maze[row][col] = VISITED
     while len(stack) > 0 and count < max:
-        directions = available_directions(row,col)
+        directions = available_directions(row,col,maze)
         n = len(directions)
         cell = None
         if n > 0:    
             i = random.randint(0,len(directions) - 1)
             cell = directions.pop(i)
             stack.append(cell)
+             # mark cell visited
+            maze[cell[0]][cell[1]] = VISITED
+            visited.append((cell[0], cell[1]))
         elif len(stack) > 0:
             cell = stack.pop()
         else:
@@ -130,24 +136,32 @@ def find_path_lv(row, col, maze, dest_row,dest_col, solution, current_path, coun
             # no solution
             return
         
-        
-        # mark cell visited
-        maze[cell[0]][cell[1]] = VISITED
-
+                          
         # If we reach the destination cell of the matrix, add
         # the current path to solution and return
         if cell[0] == dest_row and cell[1] == dest_col:
-            solution.append(current_path)
+            solution.append('S')
             return
         row = cell[0]
         col = cell[1]
         count += 1
         
 
-def print_maze(path_list):
+# Returns a copy of the maze in order 
+# to preserve the state of the original maze
+# for further runs. 
+def copy_maze():
+    return [row[:] for row in maze]
+
+# Prints the paths, the maze and the cells visited.
+# Walls are represented as squares and visited cells as dots.
+def print_maze(maze, path_list,print_is_solved=False):
+    if print_is_solved:
+        print(f"Maze is solved = {solved} : " + "".join(result))
+        
     maze_local = [row[:] for row in maze]
     row,col = start
-    maze_local[row][col] = 2
+    maze_local[row][col] = STEP
     for path in path_list:
         for step in path:
             if step == 'D':
@@ -158,10 +172,12 @@ def print_maze(path_list):
                 col = col - 1
             elif step == 'R':
                 col = col + 1
-            maze_local[row][col] = 2
+            maze_local[row][col] = STEP
+            
     
     for r,c in visited:
-        maze_local[r][c] = 5
+        if maze_local[r][c] != STEP:
+            maze_local[r][c] = VISITED
     
     maze_str = []
     for row in maze_local:
@@ -170,6 +186,11 @@ def print_maze(path_list):
 
     for r in maze_str:
         print('\t' + r)
+    
+    
+    print("\n\nVisited cells: " + str(len(visited)))
+    #print(visited)
+
 
    
 
@@ -181,15 +202,45 @@ if __name__ == '__main__':
 
     current_path = ""
 
-    choice = int(input("Maze solver:\nPress 1 to run the Las Vegas algorithm \nPress 2 to run the recursive backtracking algorithm "))
+
+    print_maze(maze,'')
+
+    choice = int(input("Maze solver:\nPress 1 to run the Las Vegas algorithm \nPress 2 to run the recursive backtracking algorithm\nPress 3 to run the LV algoritm 10000 times\nPress 4 to run the deterministic algoritm 10000 times\n"))
 
     if choice == 1:
-        find_path_lv(start[0], start[1], maze, end[0], end[1], result, current_path,0)
-    else:
-        find_path(start[0], start[1], maze, end[0], end[1], result, current_path)
-    
-    solved = len(result) > 0
-    print("Maze solved {solved} : ".join(result))
-    print_maze(result) 
+        maze_loc = copy_maze()
+        find_path_lv(start[0], start[1], maze_loc, end[0], end[1], result)
+        solved = len(result) > 0
+        print_maze(maze_loc, result,True) 
+    elif choice == 2:
+        maze_loc = copy_maze()
+        find_path(start[0], start[1], maze_loc, end[0], end[1], result, current_path)
+        solved = len(result) > 0
+        print_maze(maze_loc, result,True) 
+    elif choice == 3:
+        count = 0
+        max = 10000
+        solved_counter = 0
+        visited = []
+        for i in range(0,max):
+            maze_loc = copy_maze()
+            find_path_lv(start[0], start[1], maze_loc, end[0], end[1], result)
+            if len(result) > 0:
+               solved_counter += 1
 
+        print(f'Success rate {((solved_counter/max)*100):.5f}%')  
+    elif choice == 4:
+        count = 0
+        max = 10000
+        solved_counter = 0
+        visited = []
+        for i in range(0,max):
+            maze_loc = copy_maze()
+            find_path(start[0], start[1], maze_loc, end[0], end[1], result, current_path)
+            if len(result) > 0:
+               solved_counter += 1
+
+        print(f'Success rate {((solved_counter/max)*100):.5f}%')  
+    
+    
             
